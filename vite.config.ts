8289c74e8698bdd640b9c99bb8f97f156a2eccdd
sharun <kansharun@gmail.com>
2025-12-23 19:14:31 -0500

    disable server host

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/oregano/",
-  server: {
-    host: true
-  }
+  base: "/oregano/"
+ 
 })
 
 
